97264e7e7b000546a870689975893db30db9be53
Jake Bailey <tehsusenoh@gmail.com>
2012-03-24 09:48:56 -0500

    Add new Reddit API class.

--- src/com/redditandroiddevelopers/RedditQuickSubmit/LoginActivity.java
+++ src/com/redditandroiddevelopers/RedditQuickSubmit/LoginActivity.java
@@ -1,8 +1,6 @@
 package com.redditandroiddevelopers.RedditQuickSubmit;
 
-import java.io.IOException;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,71 +8,62 @@ import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
 import com.redditandroiddevelopers.RedditQuickSubmit.R;
 
 public class LoginActivity extends Activity {
 
-    // Start the main activity
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	
-	// Remove the title from the window; it doesn't look good. 
-	requestWindowFeature(Window.FEATURE_NO_TITLE); 
-	setContentView(R.layout.login);
-	
-	Button submitLinkButton = (Button) findViewById(R.id.submitLoginButton);
-	
-	// Get the EditTexts from the XML
-	final EditText username = (EditText) findViewById(R.id.usernameForm);
-	final EditText password = (EditText) findViewById(R.id.passwordForm);
-	
-	final String url = "https://ssl.reddit.com/api/login/" + username.getText().toString();
+	// Start the main activity
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-	submitLinkButton.setOnClickListener(new OnClickListener() {
-	    @Override
-	    public void onClick(View v) {
-		
-		/*
-		
-		// Doesn't work.. needs to be fixed
-		if (username.getText().toString() == ""){
-		    
-		    Context context = getApplicationContext();
-		    CharSequence text = "Please enter a username!";
-		    int duration = Toast.LENGTH_SHORT;
-		    Toast toast = Toast.makeText(context, text, duration);
-		    toast.show();
-		
-		// Doesn't work.. needs to be fixed
-		} else if (password.getText().toString() == ""){
-		    
-		    Context context = getApplicationContext();
-		    CharSequence text = "Please enter a password!";
-		    int duration = Toast.LENGTH_SHORT;
-		    Toast toast = Toast.makeText(context, text, duration);
-		    toast.show();
-		    
-		} else {
-		    
-                    try {
-                	LoginFunc.login(url, username.getText().toString(), password.getText().toString());
-                    } catch (IOException e) {
-                	e.printStackTrace();
-                    }
-		}
-	
-		*/
-		
-		
-		Intent myIntent = new Intent(v.getContext(),
-			RedditQuickSubmitActivity.class);
-		startActivityForResult(myIntent, 0);
-		
-	    }
-	});
-    }
+		// Remove the title from the window; it doesn't look good.
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.login);
+
+		Button submitLinkButton = (Button) findViewById(R.id.submitLoginButton);
+
+		// Get the EditTexts from the XML
+		final EditText username = (EditText) findViewById(R.id.usernameForm);
+		final EditText password = (EditText) findViewById(R.id.passwordForm);
+
+		final String url = "https://ssl.reddit.com/api/login/"
+				+ username.getText().toString();
+
+		submitLinkButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				/*
+				 * 
+				 * // Doesn't work.. needs to be fixed if
+				 * (username.getText().toString() == ""){
+				 * 
+				 * Context context = getApplicationContext(); CharSequence text
+				 * = "Please enter a username!"; int duration =
+				 * Toast.LENGTH_SHORT; Toast toast = Toast.makeText(context,
+				 * text, duration); toast.show();
+				 * 
+				 * // Doesn't work.. needs to be fixed } else if
+				 * (password.getText().toString() == ""){
+				 * 
+				 * Context context = getApplicationContext(); CharSequence text
+				 * = "Please enter a password!"; int duration =
+				 * Toast.LENGTH_SHORT; Toast toast = Toast.makeText(context,
+				 * text, duration); toast.show();
+				 * 
+				 * } else {
+				 * 
+				 * try { LoginFunc.login(url, username.getText().toString(),
+				 * password.getText().toString()); } catch (IOException e) {
+				 * e.printStackTrace(); } }
+				 */
+
+				Intent myIntent = new Intent(v.getContext(),
+						RedditQuickSubmitActivity.class);
+				startActivityForResult(myIntent, 0);
+
+			}
+		});
+	}
 }
-
--- src/com/redditandroiddevelopers/RedditQuickSubmit/RedditQuickSubmitActivity.java
+++ src/com/redditandroiddevelopers/RedditQuickSubmit/RedditQuickSubmitActivity.java
@@ -17,102 +17,105 @@ import com.redditandroiddevelopers.RedditQuickSubmit.R;
 
 public class RedditQuickSubmitActivity extends Activity {
 
-    // Create a new instace of AlertDialog called 'alertDialog' to be used later.
-    private AlertDialog.Builder alertDialog;
+	// Create a new instace of AlertDialog called 'alertDialog' to be used
+	// later.
+	private AlertDialog.Builder alertDialog;
 
-    // This is used for the AlertDialog
-    final CharSequence[] items = { "Camera", "Gallery" }; 
-    
-    // Create variables to be used by the camera intent
-    public static final int CAMERA_PIC_REQUEST = 0; 
-    public static String imageid;
-    public static Intent data;
-    public static Bitmap photo;
+	// This is used for the AlertDialog
+	final CharSequence[] items = { "Camera", "Gallery" };
 
-    // Create the camera intent be be called later when the user selects the camera
-    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+	// Create variables to be used by the camera intent
+	public static final int CAMERA_PIC_REQUEST = 0;
+	public static String imageid;
+	public static Intent data;
+	public static Bitmap photo;
 
-    
-    // Start the main activity
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	
-	// Remove the title from the window; it doesn't look good. 
-	requestWindowFeature(Window.FEATURE_NO_TITLE); 
-	setContentView(R.layout.main);
-	
-	Button submitImageButton = (Button) findViewById(R.id.submitImageButton); 
-	Button submitTextButton = (Button) findViewById(R.id.submitTextButton); 
-	Button submitLinkButton = (Button) findViewById(R.id.submitLinkButton);
+	// Create the camera intent be be called later when the user selects the
+	// camera
+	Intent cameraIntent = new Intent(
+			android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
 
-	// Create a new AlertDialog for the pop-up
-	alertDialog = new AlertDialog.Builder(this); 
-	
-	// Set its title
-	alertDialog.setTitle("Select Source"); 
+	// Start the main activity
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-	// Open pop up menu when Camera button is clicked
-	alertDialog.setItems(items, new DialogInterface.OnClickListener() {
-	    public void onClick(DialogInterface dialog, int item) {
-		if (item == 0) {
-		    startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
-		} else if (item == 1){
-		    
-		    //Display message when clicking on gallery. Someone is working on this I believe. 
-		    Context context = getApplicationContext();
-		    CharSequence text = "Gallery functionality will be added soon!";
-		    int duration = Toast.LENGTH_SHORT;
+		// Remove the title from the window; it doesn't look good.
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.main);
 
-		    Toast toast = Toast.makeText(context, text, duration);
-		    toast.show();
-		}
-	    }
-	});
-	
-	// Listener for the Image button to open the corresponding activity
-	submitImageButton.setOnClickListener(new OnClickListener() { 				     
-	    @Override
-	    public void onClick(View v) { 
-		AlertDialog myalert = alertDialog.create(); 
-		myalert.show();
-	    }
-	});
+		Button submitImageButton = (Button) findViewById(R.id.submitImageButton);
+		Button submitTextButton = (Button) findViewById(R.id.submitTextButton);
+		Button submitLinkButton = (Button) findViewById(R.id.submitLinkButton);
 
-	// Listener for the Text button to open the corresponding activity
-	submitTextButton.setOnClickListener(new OnClickListener() {
-	    @Override
-	    public void onClick(View v) {
-		Intent myIntent = new Intent(v.getContext(),
-			SubmitTextActivity.class);
-		startActivityForResult(myIntent, 0);
-	    }
-	});
+		// Create a new AlertDialog for the pop-up
+		alertDialog = new AlertDialog.Builder(this);
 
-	// Listener for the Link button to open the corresponding activity
-	submitLinkButton.setOnClickListener(new OnClickListener() {
-	    @Override
-	    public void onClick(View v) {
-		Intent myIntent = new Intent(v.getContext(),
-			SubmitLinkActivity.class);
-		startActivityForResult(myIntent, 0);
-	    }
-	});
-    }
-    
-    // This gets the result from the camera (i.e. the picture) when it is called
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	if (requestCode == CAMERA_PIC_REQUEST) {
-	    if (data != null) {
-		photo = (Bitmap) data.getExtras().get("data");
+		// Set its title
+		alertDialog.setTitle("Select Source");
 
-		Intent cameraSubmit = new Intent(
-			RedditQuickSubmitActivity.this,
-			SubmitCameraImageActivity.class);
-		startActivity(cameraSubmit);
-	    }
+		// Open pop up menu when Camera button is clicked
+		alertDialog.setItems(items, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int item) {
+				if (item == 0) {
+					startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
+				} else if (item == 1) {
+
+					// Display message when clicking on gallery. Someone is
+					// working on this I believe.
+					Context context = getApplicationContext();
+					CharSequence text = "Gallery functionality will be added soon!";
+					int duration = Toast.LENGTH_SHORT;
+
+					Toast toast = Toast.makeText(context, text, duration);
+					toast.show();
+				}
+			}
+		});
+
+		// Listener for the Image button to open the corresponding activity
+		submitImageButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				AlertDialog myalert = alertDialog.create();
+				myalert.show();
+			}
+		});
+
+		// Listener for the Text button to open the corresponding activity
+		submitTextButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(v.getContext(),
+						SubmitTextActivity.class);
+				startActivityForResult(myIntent, 0);
+			}
+		});
+
+		// Listener for the Link button to open the corresponding activity
+		submitLinkButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(v.getContext(),
+						SubmitLinkActivity.class);
+				startActivityForResult(myIntent, 0);
+			}
+		});
 	}
-    }
-}
 
+	// This gets the result from the camera (i.e. the picture) when it is called
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == CAMERA_PIC_REQUEST) {
+			if (data != null) {
+				photo = (Bitmap) data.getExtras().get("data");
+
+				Intent cameraSubmit = new Intent(
+						RedditQuickSubmitActivity.this,
+						SubmitCameraImageActivity.class);
+				startActivity(cameraSubmit);
+			}
+		}
+	}
+}
